9298dd77b6b863e08a5f7046fa6a86352b511a3d
Asal <aas047@uit.no>
2022-02-28 16:10:51 +0100

    adding loop in test

--- testing/unittests/predefined/predef_solver_dev_level2.cpp
+++ testing/unittests/predefined/predef_solver_dev_level2.cpp
@@ -284,9 +284,9 @@ struct SolverDevLevel2Step2_Fixture003 : ::testing::Test {
 
 TEST_F(SolverDevLevel2Step2_Fixture003, Test001)
 {
-  //  for (int i = 0; i < 1000; i++) {
-  solver_dev::level2::solve(*m_scenario, 16ms);
-  //  }
+  for (int i = 0; i < 1000; i++) {
+    solver_dev::level2::solve(*m_scenario, 16ms);
+  }
   // Expect to be inbetween the planes
   for (auto const& s_rid : m_scenario->nonFixedSphereRBs()) {
     for (auto const& p_rid : m_scenario->fixedInfPlaneRBs()) {
